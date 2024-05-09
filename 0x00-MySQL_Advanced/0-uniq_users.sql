1f2f0b46540c8340d1f70c7864e611293c2ae0c4
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-09 06:14:08 +0000

    Create table with unique email constraint

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,1 +1,7 @@
--- Comments are important
+-- Create table with unique constraint
+-- Have id, name and email attributes
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);

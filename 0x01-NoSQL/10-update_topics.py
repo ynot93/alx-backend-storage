19166bc9b48303b07addb9c92c9cce02b023626e
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-14 11:49:01 +0300

    Update variable of document based on query parameter

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module performs CRUD functions on MongoDB
+
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Change all topics of school document with the name
+    as the query parameter
+
+    """
+    mongo_collection.update_one(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
+    

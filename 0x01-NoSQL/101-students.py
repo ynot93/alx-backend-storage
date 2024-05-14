5854d15a3116e2764175d3cebe9f722d5463be47
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-14 20:19:10 +0300

    Use aggregate to count average

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,24 @@
+"""
+This module performs CRUD functions on MongoDB
+
+"""
+
+
+def top_students(mongo_collection):
+    """
+    Returns all students sorted by average score
+
+    """
+    pipeline = [
+        {
+            "$project": {
+                "name": 1,
+                "score": 1,
+                "averageScore": { "$avg": "topics.score" }
+            }
+        },
+        {
+            "$sort": { "averageScore": -1 }
+        }
+    ]
+    return mongo_collection.aggregate(pipeline)

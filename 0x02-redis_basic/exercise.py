8dca8eacecbd86c4c13431820f323c578adda783
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-15 23:42:31 +0300

    Retrieve objects and convert to desired format

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ This module deals with the basic operations in Redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -29,3 +29,29 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    
+    def get(self, key: str, fn: Optional[Callable] = None) -> Optional[Union[str, bytes, int, float]]:
+        """
+        Retrieves object from redis
+
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+    
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Converts object to string
+
+        """
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+    
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Converts object to integer
+
+        """
+        return self.get(key, fn=int)
